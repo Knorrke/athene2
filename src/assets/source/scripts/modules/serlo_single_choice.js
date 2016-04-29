9df37ea25981046486afb6cfda016ec04ea630ae
Knorrke <knorr.b@gmx.de>
2016-04-29 17:28:34 +0200

    new design for single choice

--- src/assets/source/scripts/modules/serlo_single_choice.js
+++ src/assets/source/scripts/modules/serlo_single_choice.js
@@ -8,11 +8,20 @@ define(['jquery'], function ($) {
             var $self = $(this);
             $self.submit(function (e) {
                 e.preventDefault();
-                var $selected = $('.single-choice-answer-choice:checked', this),
+                var $selected = $('.single-choice-answer-content.active', this),
                     $feedback = $selected.siblings('.single-choice-answer-feedback');
 
                 $('.single-choice-answer-feedback', $self).hide();
+                $('.single-choice-answer-content', $self).removeClass('btn-success').removeClass('btn-warning').addClass('button-default');
+                $('.single-choice-submit', $self).removeClass('btn-success').removeClass('btn-warning');
                 $feedback.show();
+                if (Boolean($selected.attr('val'))) {
+                    $selected.removeClass('button-default').addClass('btn-success');
+                    $('.single-choice-submit', $self).addClass('btn-success');
+                } else {
+                    $selected.removeClass('button-default').addClass('btn-warning');
+                    $('.single-choice-submit', $self).addClass('btn-warning');
+                }
                 return false;
             });
         });
